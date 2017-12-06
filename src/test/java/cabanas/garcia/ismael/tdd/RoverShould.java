7880b27022d11fea1c96d95c4a738cd3b86f1d4c
Ismael Cabañas García <ismaelcabanas@gmail.com>
2017-12-06 19:14:54 +0100

    Tests refactoring

--- src/test/java/cabanas/garcia/ismael/tdd/RoverShould.java
+++ src/test/java/cabanas/garcia/ismael/tdd/RoverShould.java
@@ -1,16 +1,22 @@
 package cabanas.garcia.ismael.tdd;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RoverShould {
 
+    private Rover rover;
+
+    @Before public void
+    setUp() {
+        rover = new Rover();
+    }
+
     @Test
     public void
     startup_in_initial_position() {
-        Rover rover = new Rover();
-
         assertThat(rover.getPosX()).isEqualTo(0);
         assertThat(rover.getPosY()).isEqualTo(0);
         assertThat(rover.getOrientation()).isEqualTo("N");
@@ -18,8 +24,6 @@ public class RoverShould {
 
     @Test public void
     rotate_to_right() {
-        Rover rover = new Rover();
-
         rover.execute("R");
 
         assertThat(rover.getPosX()).isEqualTo(0);
@@ -29,8 +33,6 @@ public class RoverShould {
 
     @Test public void
     rotate_twice_to_right() {
-        Rover rover = new Rover();
-
         rover.execute("RR");
 
         assertThat(rover.getPosX()).isEqualTo(0);
