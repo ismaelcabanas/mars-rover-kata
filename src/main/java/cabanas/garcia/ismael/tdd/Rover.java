30daac1141145c5ff0a78458c14e676bad0caa4c
Ismael Cabañas García <ismaelcabanas@gmail.com>
2017-12-06 18:57:08 +0100

    Passing the start position test

---
+++ src/main/java/cabanas/garcia/ismael/tdd/Rover.java
@@ -0,0 +1,15 @@
+package cabanas.garcia.ismael.tdd;
+
+public class Rover {
+    public int getPosX() {
+        return 0;
+    }
+
+    public int getPosY() {
+        return 0;
+    }
+
+    public String getOrientation() {
+        return "N";
+    }
+}
