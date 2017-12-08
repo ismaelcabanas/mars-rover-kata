7fdde963cc7e233c68c980470c0fdc110e6bad58
Ismael Cabañas García <ismaelcabanas@gmail.com>
2017-12-08 19:55:39 +0100

    Refactoring test name

--- src/test/java/cabanas/garcia/ismael/tdd/RoverShould.java
+++ src/test/java/cabanas/garcia/ismael/tdd/RoverShould.java
@@ -118,7 +118,7 @@ public class RoverShould {
     @Parameters({
             "MMMMMMMMMM, 0, 0, N"
     }) public void
-    return_to_initial_position_when_reach_top_of_the_grid(String commands, int expectedPosX, int expectedPosY,
+    wrap_from_top_to_bottom_when_moving_to_norht(String commands, int expectedPosX, int expectedPosY,
                                                           String expectedOrientation) {
         rover.execute(commands);
 
@@ -126,4 +126,5 @@ public class RoverShould {
         assertThat(rover.getPosY()).isEqualTo(expectedPosY);
         assertThat(rover.getOrientation()).isEqualTo(expectedOrientation);
     }
+
 }
